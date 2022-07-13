d756e717b323d6d7afe5b92be2a38a4ba0f9f427
wlQkr <wlqkr1010@gmail.com>
2022-07-13 16:50:00 +0900

    searchBar UI 추가

--- Movie/Scenes/MovieListPresenter.swift
+++ Movie/Scenes/MovieListPresenter.swift
@@ -5,13 +5,14 @@
 //  Created by 박지용 on 2022/07/12.
 //
 
-import Foundation
+import UIKit
 
 protocol MovieListProtocol: AnyObject {
     func setupNavigationBar()
+    func setupSearchBar()
 }
 
-final class MovieListPrenseter {
+final class MovieListPrenseter: NSObject {
     // weak var를 사용하면 메모리 참조에서 안전하기 때문에 화면이 생성됚다가 사라지는게 빈번하면 memory leak에 위험이 있기 때문에 weak var나 unwned let을 사용하면 더 안전한 코드로 구성이 가능함
     private weak var viewController: MovieListProtocol?
     
@@ -21,5 +22,8 @@ final class MovieListPrenseter {
     
     func viewDidLoad() {
         viewController?.setupNavigationBar()
+        viewController?.setupSearchBar()
     }
 }
+
+extension MovieListPrenseter: UISearchBarDelegate {}
--- Movie/Scenes/MovieListViewController.swift
+++ Movie/Scenes/MovieListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class MovieListViewController: UIViewController {
     private lazy var presenter = MovieListPrenseter(viewController: self)
     
+    private let searchController = UISearchController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,4 +25,13 @@ extension MovieListViewController: MovieListProtocol {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
     }
+    
+    func setupSearchBar() {
+        // searchBar 활설 시 배경색 어둡게
+        searchController.obscuresBackgroundDuringPresentation = false
+        // searchBar의 자동완성을 사용하기 위해서는 delegate를 사용해야 한다.
+        searchController.searchBar.delegate = presenter
+        navigationItem.searchController = searchController
+    }
+    
 }
