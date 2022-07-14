b26bdcb6bc3c267ab8565ebd0bfa940f893d52fa
wlQkr <wlqkr1010@gmail.com>
2022-07-14 20:55:48 +0900

    customCell autolayout, label, image 추가 등등

--- Movie/Models/Movie.swift
+++ Movie/Models/Movie.swift
@@ -16,4 +16,19 @@ struct Movie: Decodable {
     let pubDate: String
     
     var imageURL: URL? { URL(string: image) }
+    
+    init(title: String,
+         imageURL: String,
+         userRating: String,
+         actor: String,
+         director: String,
+         pubDate: String
+    ) {
+        self.title = title
+        self.image = imageURL
+        self.userRating = userRating
+        self.actor = actor
+        self.director = director
+        self.pubDate = pubDate
+    }
 }
--- Movie/Scenes/MovieList/MovieListPresenter.swift
+++ Movie/Scenes/MovieList/MovieListPresenter.swift
@@ -17,6 +17,12 @@ final class MovieListPrenseter: NSObject {
     // weak var를 사용하면 메모리 참조에서 안전하기 때문에 화면이 생성됚다가 사라지는게 빈번하면 memory leak에 위험이 있기 때문에 weak var나 unwned let을 사용하면 더 안전한 코드로 구성이 가능함
     private weak var viewController: MovieListProtocol?
     
+    private var likedMovie: [Movie] = [
+        Movie(title: "Topgun", imageURL: "", userRating: "10.0", actor: "123", director: "123", pubDate: "2022"),
+        Movie(title: "Topgun", imageURL: "", userRating: "10.0", actor: "123", director: "123", pubDate: "2022"),
+        Movie(title: "Topgun", imageURL: "", userRating: "10.0", actor: "123", director: "123", pubDate: "2022")
+    ]
+    
     init(viewController: MovieListProtocol) {
         self.viewController = viewController
     }
@@ -44,7 +50,7 @@ extension MovieListPrenseter: UICollectionViewDelegateFlowLayout {
 }
 extension MovieListPrenseter: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return likedMovie.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -53,7 +59,8 @@ extension MovieListPrenseter: UICollectionViewDataSource {
             withReuseIdentifier: MovieListCollectionViewCell.identifier,
             for: indexPath) as? MovieListCollectionViewCell
         
-        cell?.backgroundColor = .green
+        let movie = likedMovie[indexPath.item]
+        cell?.update(movie)
         
         return cell ?? UICollectionViewCell()
     }
